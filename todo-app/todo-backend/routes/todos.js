e3467c8734e7f1dcf778387d0e5b315c9d925b46
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-01-13 14:53:04 +0000

    implemented singleRouter PUT and GET todo routes with tests in requests/requests.rest

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,20 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = await req.todo
+  res.send(todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = await req.todo
+  try {
+    todo.text = req.body.text
+    await todo.save()
+    res.send(todo)
+  } catch(e) {
+    res.send(400)
+  }
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
